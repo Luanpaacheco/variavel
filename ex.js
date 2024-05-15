d9c0e9d5cb6106f9361cb80421448b4276e9d226
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2024-05-15 19:58:31 -0300

    brincando em js

---
+++ ex.js
@@ -0,0 +1,14 @@
+const nome="luan"
+const sobrenome ="pacheco"
+const idade=20
+const peso=72
+const alturaEmM=1.8
+let indiceDeMassaCorporal;
+let anoDeNascimento;
+
+indiceDeMassaCorporal = peso/(alturaEmM*alturaEmM)
+anoDeNascimento= 2024-idade
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg`)
+console.log(`tem ${alturaEmM} de altura e seu imc é de  ${indiceDeMassaCorporal}`)
+console.log(`${nome} nasceu em ${anoDeNascimento}`)
